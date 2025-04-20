fffac7f46871c0439756f036ab79d83fb70b3e7c
agus <agustinolivieri30@gmail.com>
2025-04-20 12:49:39 -0300

    changed bg gradient start

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,12 @@ import Welcome from '@/components/Welcome.vue';
     <div
       class="absolute top-0 bottom-0 z-[-2] min-h-screen w-full bg-gray-50 dark:bg-gray-950
       bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(217,216,255,0.5),rgba(255,255,255,0.9))]
-      dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
+      dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-28%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
     ></div>
 
     <PortfolioHeader />
-    <Welcome />
-    <div class="h-screen">
-      <h1>Bueeeep</h1>
-
-    </div>
+    <main>
+      <Welcome />
+    </main>
 </template>
 
