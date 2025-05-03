258aced936a901893b85403894b18f11000eb311
agus <agustinolivieri30@gmail.com>
2025-05-03 20:24:48 -0300

    changed title in welcome section

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -5,7 +5,7 @@
 <template>
     <img src="../assets/images/owner.jpg" class="rounded-full w-24 h-21" />
     <div class="">
-        <h1 class="text-5xl font-bold mb-5">Hi, I am a <span class="dev-animation text-yellow-400"></span></h1>
-        <h2 class="text-xl">Take a look at my <span class="secundary-color">portfolio!</span></h2>
+        <h1 class="text-5xl font-bold mb-6 flex flex-col">Hi, I am a <span class="dev-animation text-yellow-400"></span></h1>
+        <h2 class="text-xl text-blue-400">Take a look at my portfolio!</h2>
     </div>
 </template>
