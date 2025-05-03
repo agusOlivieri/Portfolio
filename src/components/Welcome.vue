4ec95645c7a64f3be65f63132d76dfde56085d0e
agus <agustinolivieri30@gmail.com>
2025-05-03 20:19:05 -0300

    added owner photo in the welcome section

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -3,14 +3,7 @@
 </script>
 
 <template>
-    <!-- <section id="welcome-section" class="h-screen max-w-5xl mx-auto px-22 flex flex-col justify-center items-start gap-5">
-        <div class="bg-white size-16 rounded-full"></div>
-        <div class="">
-            <h1 class="text-5xl font-bold mb-5">Hi, I am a <span class="dev-animation text-yellow-400"></span></h1>
-            <h2 class="text-xl">Take a look at my <span class="secundary-color">portfolio!</span></h2>
-        </div>
-    </section> -->
-    <div class="bg-white size-16 rounded-full"></div>
+    <img src="../assets/images/owner.jpg" class="rounded-full w-24 h-21" />
     <div class="">
         <h1 class="text-5xl font-bold mb-5">Hi, I am a <span class="dev-animation text-yellow-400"></span></h1>
         <h2 class="text-xl">Take a look at my <span class="secundary-color">portfolio!</span></h2>
